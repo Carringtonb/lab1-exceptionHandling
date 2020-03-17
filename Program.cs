05caa27831e23ec1d1e9907705c944e6caed0196
carrington <carringtonbeard@live.com>
2020-03-17 11:04:38 -0700

    added gitignore

--- Program.cs
+++ Program.cs
@@ -7,7 +7,40 @@ namespace Lab_01_exceptionHandling
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            StartSequence();
         }
+        static void StartSequence()
+		{
+			try
+			{
+            Console.WriteLine("Enter a number greater than zero");
+            string inputNumber = Console.ReadLine();
+            int numberConverted = Convert.ToInt32(inputNumber);
+
+            Populate(Array);
+            GetSum(Array);
+                GetProduct(Array, GetSum(Array));
+                GetQuotient(GetProduct);
+			}
+            catch (FormatException e)
+			{
+                Console.WriteLine(e.Message);
+			}
+            catch (OverflowException e)
+			{
+                Console.WriteLine(e.Message);
+			}
+		}
+        static int[] Populate(int[] populateArray)
+		{
+            
+                for(int i = 0; i < populateArray.Length; i++)
+			{
+                Console.WriteLine($"Enter a number, {i} of {populateArray.length}");
+                string userInput = Console.ReadLine();
+                int convertedInput = Convert.ToInt32(userInput);
+                populateArray[i] = convertedInput;
+			}
+		}
     }
 }
