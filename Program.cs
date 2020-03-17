c49ee0fdbaf09d8548f9492429b940a2847b37c7
carrington <carringtonbeard@live.com>
2020-03-17 12:27:07 -0700

    updated messages

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Lab_01_exceptionHandling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to my game! Let's do some math!");
             StartSequence();
         }
         static void StartSequence()
@@ -20,7 +20,7 @@ namespace Lab_01_exceptionHandling
             Populate(Array);
             GetSum(Array);
                 GetProduct(Array, GetSum(Array));
-                GetQuotient(GetProduct);
+                GetQuotient(GetProduct(Array, GetSum(Array)));
 			}
             catch (FormatException e)
 			{
@@ -31,6 +31,7 @@ namespace Lab_01_exceptionHandling
                 Console.WriteLine(e.Message);
 			}
 		}
+
         static int[] Populate(int[] populateArray)
 		{
             
@@ -41,6 +42,61 @@ namespace Lab_01_exceptionHandling
                 int convertedInput = Convert.ToInt32(userInput);
                 populateArray[i] = convertedInput;
 			}
+            return populateArray;
+		}
+
+        static int[] GetSum(int[] sumArray)
+		{
+            int sum = 0;
+            for (int i = 0; i < sumArray.length; i++)
+			{
+                sum += Array[i];
+			}
+                if (sum < 20)
+				{
+                    Console.WriteLine($"Value of {sum} is too low.")
+				}
+            return sumArray;
+		}
+
+        static int GetProduct(int[] productArr, int sum)
+		{
+			try
+			{
+                Console.WriteLine($"Enter a number between 1 and {productArr.length}");
+                string userInput = Console.ReadLine();
+                int convertedInput = Convert.ToInt32(userInput);
+
+                var product = sum * productArr[convertedInput];
+
+                return product;
+            }
+			catch (IndexOutOfRangeException ex)
+			{
+                Console.WriteLine(ex.Message);
+                throw;
+			}
+     
+		}
+
+        static decimal GetQuotient(int product)
+		{
+			try
+			{
+                Console.WriteLine("Please enter a number to divide your product by");
+                string input = Console.ReadLine();
+
+                int divideNum = Convert.ToInt32(input);
+
+                Decimal value = decimal.Divide(divideNum, product);
+
+                return value;
+            }
+			catch (DivideByZeroException ex)
+			{
+                return 0;
+			}
+       
 		}
     }
 }
